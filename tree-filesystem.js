5bbc1de6ac8e50e7f166c837a0ed63966db26440
Eric <84822642+erichen114@users.noreply.github.com>
2022-07-01 15:57:08 -0700

    add filesystem tree

--- tree-filesystem.js
+++ tree-filesystem.js
@@ -7,46 +7,71 @@ class Node {
 
   addNode(value) {
     const segments = value.split("/");
-    // ['documents', 'personal', 'chickenshit.exe']
+
     if (segments.length === 0) return;
+
     if (segments.length === 1) {
       const node = new Node(segments[0], this);
       this.children.push(node);
       return { node: node, index: this.children.length - 1 };
     }
 
-    const existingChildNode = this.children.find(child => child === segments[0])
-    if(existingChildNode) existingChildNode.addNode(segments.slice(1).join('/'))
+    const existingChildNode = this.children.find(
+      (child) => child.value === segments[0]
+    );
+    if (existingChildNode) existingChildNode.addNode(segments.slice(1).join('/'));
     else {
+      //if there is
       const node = new Node(segments[0], this);
-      node.addNode(segments.slice(1).join('/'));
-      this.children.push(node);
-      return { node: node, index: this.children.length - 1}
+      node.addNode(segments.slice(1).join("/"));
+      this.children.push(node)
+      return { node: node, index: this.childrenlength - 1}
     }
-    
   }
 
-  removeNode(index) {
-    this.children.splice(index, 1);
+  removeNode(value) {
+    const segments = value.split('/');
+
+    if(segments.length === 0) return;
+
+    if(segments.length === 1) {
+      const existingNodeIndex = this.children.findIndex(child => child.value === segments[0])
+      if(existingNodeIndex < 0) throw new Error('Could not find matching value!')
+      this.children.splice(existingNodeIndex, 1)
+      return { removedNode: existingNodeIndex }
+    }
+ 
+    if(segments.length > 1) {
+      const existingChildNode = this.children.find(child => child.value === segments[0]);
+      if(!existingChildNode) throw new Error('Could not find matching path! Path segment: ' + segments[0])
+      
+      existingChildNode.removeNode(segments.slice(1).join('/'))
+    }
+  
   }
 }
 
 class Tree {
   constructor(rootValue) {
+    //first node we create when create a tree, then we can add children to it
     this.root = new Node(rootValue);
   }
-
   add(path) {
     this.root.addNode(path);
   }
+
+  remove(path) {
+    this.root.removeNode(path)
+  }
 }
 
-//create a tree
-//the tree nakes a node
-//each node has children
 const fileSystem = new Tree("/");
-fileSystem.add("documents");
-fileSystem.add("documents/personal/chickenshit.docx");
-fileSystem.add("games/cod.exe");
-fileSystem.add("games/cod2.exe");
-console.log(fileSystem);
+fileSystem.add('documents')
+fileSystem.add('documents/personal/tax.docx');
+fileSystem.add('games/code.exe')
+fileSystem.add('games/code2.exe');
+fileSystem.remove('games/code.exe')
+fileSystem.remove('games')
+
+
+console.log(fileSystem)
