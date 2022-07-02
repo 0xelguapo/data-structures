89bc96a90d895ade59d9674d5010de80a80d076a
Eric <84822642+erichen114@users.noreply.github.com>
2022-07-01 17:34:04 -0700

    breadth first and depth first search

---
+++ app.js
@@ -0,0 +1,103 @@
+class Node {
+  constructor(value, parentNode = null) {
+    this.children = [];
+    this.value = value;
+    this.parent = parentNode;
+  }
+
+  addNode(value) {
+    const segments = value.split("/");
+
+    if (segments.length === 0) return;
+
+    if (segments.length === 1) {
+      const node = new Node(segments[0], this);
+      this.children.push(node);
+      return { node: node, index: this.children.length - 1 };
+    }
+
+    const existingChildNode = this.children.find(
+      (child) => child.value === segments[0]
+    );
+    if (existingChildNode)
+      existingChildNode.addNode(segments.slice(1).join("/"));
+    else {
+      //if there is
+      const node = new Node(segments[0], this);
+      node.addNode(segments.slice(1).join("/"));
+      this.children.push(node);
+      return { node: node, index: this.childrenlength - 1 };
+    }
+  }
+
+  removeNode(value) {
+    const segments = value.split("/");
+
+    if (segments.length === 0) return;
+
+    if (segments.length === 1) {
+      const existingNodeIndex = this.children.findIndex(
+        (child) => child.value === segments[0]
+      );
+      if (existingNodeIndex < 0)
+        throw new Error("Could not find matching value!");
+      this.children.splice(existingNodeIndex, 1);
+      return { removedNode: existingNodeIndex };
+    }
+
+    if (segments.length > 1) {
+      const existingChildNode = this.children.find(
+        (child) => child.value === segments[0]
+      );
+      if (!existingChildNode)
+        throw new Error(
+          "Could not find matching path! Path segment: " + segments[0]
+        );
+
+      existingChildNode.removeNode(segments.slice(1).join("/"));
+    }
+  }
+
+  find(value) {
+    //Breadth-first approach
+    //first we check the children of the first node
+    for (const child of this.children) {
+      if (child.value === value) return child;
+    }
+
+    //then if we don't find anything we move onto the next node 
+    for (const child of this.children) {
+      const nestedChildNode = child.find(value);
+      if (nestedChildNode) return nestedChildNode;
+    }
+  }
+}
+
+class Tree {
+  constructor(rootValue) {
+    //first node we create when create a tree, then we can add children to it
+    this.root = new Node(rootValue);
+  }
+  add(path) {
+    this.root.addNode(path);
+  }
+
+  remove(path) {
+    this.root.removeNode(path);
+  }
+
+  find(value) {
+    if (this.root.value === value) return this.root;
+    return this.root.find(value);
+  }
+}
+
+const fileSystem = new Tree("/");
+fileSystem.add("documents");
+fileSystem.add("documents/personal/tax.docx");
+fileSystem.add("games/code.exe");
+fileSystem.add("games/code2.exe");
+fileSystem.remove("games/code.exe");
+console.log(fileSystem.find("personal"));
+
+console.log(fileSystem);
