74d8512f77414b0b3d8f525fc5876ac00ee00b6b
Eric <84822642+erichen114@users.noreply.github.com>
2022-07-20 10:40:31 -0700

    add power and prime

--- algorithms/app.js
+++ algorithms/app.js
@@ -15,3 +15,31 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(6))//0, 1, 1, 2, 3, 5, 8
+
+function isPrime(num) {
+  for(let i = 2; i < Math.sqrt(num); i++) {
+    if(num % i === 0) return false
+  }
+  return true;
+}
+
+console.log(isPrime(27277))
+
+function smallestValue(arr) {
+  let smallest = arr[0]
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] < arr[i + 1] && arr[i] <= smallest) smallest = arr[i]
+  }
+  return smallest
+}
+
+console.log(smallestValue([3, 5, 9, 12, 18, 2, 92])) // O(n)
+
+function isPowerOfTwo(num) {
+  if(num === 1) return true
+  if(num % 2 !== 0) return false
+  return isPowerOfTwo(num / 2)
+}
+
+console.log(isPowerOfTwo(8))
+console.log(isPowerOfTwo(12))
