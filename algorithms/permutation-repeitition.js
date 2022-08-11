4a6e800f5d9bf7ca958514d5f9e1336e27db5f83
Eric <84822642+erichen114@users.noreply.github.com>
2022-08-11 14:50:16 -0700

    add pemrutations

---
+++ algorithms/permutation-repeitition.js
@@ -0,0 +1,29 @@
+// permutations with repetitions
+// split into smaller problems. derive combinations of length 1 and combine with extra digits to derive possible 2 element combinations
+// start with simple arrays and add more combinations
+
+function getPermutations(options, length) {
+  const permutations = [];
+
+  if (length === 1) return options.map((option) => [option]);
+
+  const partialPermutations = getPermutations(options, length - 1);
+  // [[1], [2], [3]] - most deeply nested function call
+  // combine these length of 1 arrays with all other options
+
+  for (const option of options) {
+    console.log('OPTION', option)
+    for (const existingPermutation of partialPermutations) {
+      console.log('EXISTING', existingPermutation)
+      permutations.push([option].concat(existingPermutation))
+      console.log('PERMUTATIONSARRAY', permutations)
+    }
+  }
+
+  return permutations;
+}
+
+const digits = [1, 2, 3];
+const resultLength = 3;
+
+console.log(getPermutations(digits, resultLength));
